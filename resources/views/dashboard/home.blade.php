852e4d7cbeec1457b2f41087a021bcc25ef0ac7b
ilmiza <ilmizafitrah123@gmail.com>
2025-01-10 22:31:57 +0700

    vite

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel</title>
+    @vite(['resources/css/app.Scss', 'resources/js/app.js'])
 </head>
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
